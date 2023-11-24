598d39b1f9e95fc09594bda8d45b79909ae53af7
Wivson Machado <wivsonmachado@gmail.com>
2023-11-24 19:11:53 -0300

    Iniciando backend Spring Boot

---
+++ src/test/java/br/com/wmcodes/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wmcodes.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
